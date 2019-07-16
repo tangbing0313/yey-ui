67643ddb1cae318c210795138a4cec8c77ce15f0
tangbing <tangbing@chinasosmart.com>
2019-07-16 20:44:47 +0800

    a

--- src/pages/member/Fans.jsx
+++ src/pages/member/Fans.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import classNames from 'classnames';
+import styles from './index.less';
+import { Button, Card, Col, Form, Icon, List, Row, Select, Tag } from 'antd';
+
+const cls = classNames(styles.standardFormRow, {
+  [styles.standardFormRowBlock]: "block",
+  [styles.standardFormRowLast]: "last",
+  [styles.standardFormRowGrid]: "grid",
+});
+
+
 export default () => (
-  <p
-    style={{
-      textAlign: 'center',
-    }}
-  >
-    粉丝管理
-  </p>
+  <>
+  <Card bordered={false}>
+    <Form layout="inline">
+      <div className={cls}>
+        dsffdsfd
+      </div>
+      <div className={cls}>
+        dsffdsfd
+      </div>
+    </Form>
+  </Card>
+  </>
 );
