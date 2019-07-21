cacd949e1f43f05832f3355eb1c0d0d2eae2ee91
tangbing <tangbing0313@vip.qq.com>
2019-07-22 00:33:48 +0800

    e

--- src/pages/member/Fans.jsx
+++ src/pages/member/Fans.jsx
@@ -1,28 +1,51 @@
 import React from 'react';
 import styles from './Fans.less';
-import { Icon, Card, Col, Form, List, Row, Select, Tag } from 'antd';
+import { Icon, Card, Input, Radio, Button, Form, List, Tag } from 'antd';
 import FansSearch from '../components/fans_search/fans_search';
-
-console.log(styles);
+import TagSelect from '../list/search/articles/components/TagSelect';
+const { Search } = Input;
+const FormItem = Form.Item;
 export default () => (
   <>
     <div className={styles.pagehead}>
-      <h4 className={styles.h4}>
-        用户列表{' '}
-        <span>
-          当前共筛选出 <b>123</b> 名用户 <Icon type="loading" />
-        </span>
-      </h4>
+      <h4 className={styles.h4}>用户列表</h4>
       <div className={styles['pagehead-content']}>
         <span>
           当前共筛选出 <b>123</b> 名用户
         </span>
+        <span className={styles.gaojisousuo}>
+          <Search
+            placeholder="您可以输入粉丝名称/粉丝别名进行搜索"
+            onSearch={value => console.log(value)}
+            enterButton
+            style={{ width: 300 }}
+          />
+          <Button className={styles.gjss_button}>
+            高级搜索 <Icon type="down" />
+          </Button>
+        </span>
       </div>
     </div>
-    <Card bordered={false}>
+    <Card bordered={false} className={styles.ggss_box}>
       <Form layout="inline">
-        <div className={styles.fans_row}>高级筛选</div>
-        <div className={styles.fans_row}></div>
+        <div className={styles.fans_row}>
+          <div className={styles.sslabel}>
+            <span>性别</span>
+          </div>
+          <div className={styles.sscontentcontent}>
+            <FormItem>
+              <TagSelect
+                onChange={a => {
+                  console.log(a);
+                }}
+              >
+                <TagSelect.Option value="cat1">男</TagSelect.Option>
+                <TagSelect.Option value="cat2">女</TagSelect.Option>
+                <TagSelect.Option value="cat3">未知</TagSelect.Option>
+              </TagSelect>
+            </FormItem>
+          </div>
+        </div>
       </Form>
     </Card>
   </>
